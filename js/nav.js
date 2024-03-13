f41de204082a4b500a82f1549b8e14181681b78e
Mohamed Hedi Sekma <sekma.mohamed.hedi@gmail.com>
2024-03-14 00:56:42 +0100

    nav bar & header

--- js/nav.js
+++ js/nav.js
@@ -42,6 +42,23 @@ $(document).ready(function () {
     service("project-3", "project-3");
     service("project-4", "project-4");
 
+    //projects
+    //affichage du project-scroll
+    service("about", "about-scroll");
+    //conserver l'affichage du project-scroll lors du survol sur elle meme
+    service("about-scroll", "about-scroll");
+
+     //affichage des projets
+    service("about-scroll-item-1", "about-1");
+    service("about-scroll-item-2", "about-2");
+    service("about-scroll-item-3", "about-3");
+    service("about-scroll-item-4", "about-4");
+    //conserver affichage des projets
+    service("about-1", "about-1");
+    service("about-2", "about-2");
+    service("about-3", "about-3");
+    service("about-4", "about-4");
+
     //burger toggler
 $(".toggler-burger").click(function (e) { 
     e.preventDefault();
